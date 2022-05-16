faa523f27d89a03938ad324b0496ca3707579f8a
DanielEllacott1991 <102864425+DanielEllacott1991@users.noreply.github.com>
2022-05-16 15:00:21 +0100

    upload 28

--- Assets/Scripts/gamecontroller.cs
+++ Assets/Scripts/gamecontroller.cs
@@ -38,14 +38,10 @@ public class gamecontroller : MonoBehaviour
 
     IEnumerator HideMessage()
     {
-        //Print the time of when the function is first called.
-        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+   
 
         //yield on a new YieldInstruction that waits for 5 seconds.
         yield return new WaitForSeconds(4);
-
-        //After we have waited 5 seconds print the time again.
-        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
 
         GameObject.FindGameObjectWithTag("messagefoundinfo").GetComponent<Text>().text = "";
 
